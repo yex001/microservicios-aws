287309f76d651e72d39f7e22678641f99b9237cb
Yexalen <ycruzh001@gmail.com>
2023-06-24 11:41:46 -0600

    Prueba con interfaz sencilla

---
+++ code.js
@@ -0,0 +1,54 @@
+document.getElementById('registro-formulario').addEventListener('submit', function(event) {
+    event.preventDefault();  // Evita que el formulario se envíe automáticamente
+  
+    // Obtén los datos del formulario
+    const formData = new FormData(event.target);
+    const userData = {
+      name: formData.get('name'),
+      username: formData.get('username'),
+      password: formData.get('password')
+    };
+  
+    fetch('https://kk9kpglwj1.execute-api.us-east-2.amazonaws.com/register', {
+        method: 'POST',
+        body: JSON.stringify(userData)
+      })
+      .then(response => response.json())
+      .then(data => {
+        alert(data.message);  // Muestra el mensaje de la respuesta en un alert
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+    });
+
+document.getElementById('ingresar-formulario').addEventListener('submit', function(event) {
+    event.preventDefault();  
+  
+    const formData = new FormData(event.target);
+    const userData = {
+      username: formData.get('username'),
+      password: formData.get('password')
+    };
+    
+
+    fetch('https://kk9kpglwj1.execute-api.us-east-2.amazonaws.com/login', {
+        method: 'POST',
+        body: JSON.stringify(userData)
+    })
+    .then(response => response.json())
+    .then(result => {
+        if (result.statusCode === 200) {
+        // Redirigir al usuario a otra página en caso de inicio de sesión exitoso
+        localStorage.setItem('username', userData.username);
+        window.location.href = './juego.html';
+        } else {
+        // Mostrar un mensaje de alerta en caso de error de inicio de sesión
+        alert(result.body.message);
+        }
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+});
+
---
+++ juego.js
@@ -0,0 +1,48 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Obtener elementos del DOM
+    const numberInput = document.querySelector('input[type="text"]');
+    const playButton = document.querySelector('button');
+    const saldoElement = document.querySelector('p');
+  
+    // Obtener el nombre de usuario del localStorage
+    const username = localStorage.getItem('username');
+  
+    // Función para llamar a la función Lambda
+    function llamarLambda(numero, usuario) {
+      const userData = {
+        username: usuario,
+        number: numero
+      };
+  
+      fetch('https://kk9kpglwj1.execute-api.us-east-2.amazonaws.com/play', {
+        method: 'POST',
+        body: JSON.stringify(userData)
+      })
+      .then(response => response.json())
+      .then(result => {
+        // Actualizar saldo y mostrar mensaje
+        saldoElement.textContent = 'Saldo: $' + result.saldo;
+        alert(result.message);
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+    }
+  
+    // Evento de escucha para el botón "Jugar"
+    playButton.addEventListener('click', function() {
+      const numero = parseInt(numberInput.value);
+  
+      if (isNaN(numero)) {
+        alert('Por favor, ingrese un número válido');
+        return;
+      }
+  
+      llamarLambda(numero, username);
+    });
+  
+    // Mostrar el nombre de usuario y saldo inicial
+    document.querySelector('.username').textContent = 'Usuario: ' + username;
+    saldoElement.textContent = 'Saldo: $0';
+  });
+  
